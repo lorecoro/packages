9e4ef0616df3f6853597665da4830b0c7b09dd44
Lorenzo <lorenzo@coronati.com>
2023-12-22 22:04:53 +0200

    chapter 7 latest invoices

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,7 +2,8 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
-export default async function LatestInvoices() {
+export default async function LatestInvoices(latestInvoicesObj) {
+  const { latestInvoices } = latestInvoicesObj;
   return (
     <div className="flex w-full flex-col md:col-span-4 lg:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
